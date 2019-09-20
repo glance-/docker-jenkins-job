5a5367f96e9db783d821d23606da65e4038fa5dc
Anton Lundin <anton.lundin@umu.se>
2019-09-20 12:57:41 +0200

    Adapt docker inspect template to modern docker
    In moden docker the data structure looks a bit different, to support
    multiple different networks.
    This adapts the inspect command to work on a modern docker server.

--- node/run.sh
+++ node/run.sh
@@ -5,7 +5,7 @@ set -x
 
 ## Commands to run on container start
 # Allow tests to connect to containers as they where on localhost
-echo $(docker inspect -f '{{.NetworkSettings.Gateway}}' $(hostname)) localhost >> /etc/hosts
+echo "$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.Gateway}}{{end}}' "$(hostname)") localhost" >> /etc/hosts
 
 # CloudBees standard "wait for exec" command
 /bin/cat
